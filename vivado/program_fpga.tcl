b8d8b8dfa9684a4e2222872da6224bec9e31bb7b
buffersquid <mail@buffersquid.com>
2025-07-08 19:38:18 -0700

    Modify program_fpga.tcl to use dynamic top file

--- vivado/program_fpga.tcl
+++ vivado/program_fpga.tcl
@@ -3,11 +3,13 @@ set src_dir "$proj_dir/src"
 set constr_dir "$proj_dir/constr"
 set build_dir "$proj_dir/build"
 
+set top [lindex $argv 0]
+
 open_hw_manager
 connect_hw_server
 open_hw_target
 current_hw_device [lindex [get_hw_devices] 0]
 refresh_hw_device -update_hw_probes false [current_hw_device]
 
-set_property PROGRAM.FILE "build/top.bit" [current_hw_device]
+set_property PROGRAM.FILE $build_dir/$top.bit [current_hw_device]
 program_hw_devices [current_hw_device]
