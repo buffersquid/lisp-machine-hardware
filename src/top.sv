d630bee553343be1dd38f2f4fcd8a272c315d5fb
buffersquid <mail@buffersquid.com>
2025-07-13 17:17:37 -0700

    Allow for user to input initial expression via switches

--- src/top.sv
+++ src/top.sv
@@ -1,5 +1,7 @@
 module top (
   input  wire         CLK100MHZ,
+  input  wire  [ 4:0] BTN,
+  input  wire  [15:0] SWITCHES,
   output logic [15:0] LEDS,
   output logic [ 7:0] CATHODES,
   output logic [ 3:0] ANODES
@@ -7,6 +9,8 @@ module top (
 
   core my_core (
     .clk(CLK100MHZ),
+    .btn_start(BTN[0]),
+    .switches(SWITCHES),
     .leds(LEDS),
     .cathodes(CATHODES),
     .anodes(ANODES)
